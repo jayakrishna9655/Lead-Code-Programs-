c30feaa28d1dea07a51a1f7fcd6a2ce956ec6100
JAYAKRISHNA T <jayakrishna96555@gmail.com>
2025-09-08 22:47:10 +0530

    Add files via upload

---
+++ FindLargeArrayIndex.java
@@ -0,0 +1,21 @@
+package Programs;
+
+public class FindLargeArrayIndex {
+
+	public static void main(String[] arg) {
+		
+		int[] array= {1,2,3,4,5,6,7,8,9,10};
+		
+		int max= array[0];
+		int index=0;
+		
+		for(int i = 1 ;i<array.length;i++) {
+			if(array[i]>max) {
+				max=array[i];
+			}
+		}
+		System.out.println(max);
+		
+	}
+	
+}
