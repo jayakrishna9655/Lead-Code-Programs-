39a3552e2cc60bc6b6152bf91d80de49cb8fc75d
JAYAKRISHNA T <jayakrishna96555@gmail.com>
2025-10-14 23:08:32 +0530

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,17 @@
+package Programs;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str="jai";
+		String rev="";
+		for(int i =0;i<str.length();i++) {
+			rev=str.charAt(i)+rev;
+		}
+		System.out.println(rev);
+		
+	}
+
+}
